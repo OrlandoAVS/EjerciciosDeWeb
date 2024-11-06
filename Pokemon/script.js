5f416e4ac6961f8e76a4dea453c11e876ebbdf2d
OrlandoAVS <00227722@uca.edu.sv>
2024-11-06 09:49:50 -0600

    :memo:Agregando el boton Detele

--- Pokemon/script.js
+++ Pokemon/script.js
@@ -1,3 +1,4 @@
+//Boton Envio
 const sendButton = document.querySelector(".pokemon button");
 sendButton.addEventListener("click", funcionCaja);
 
@@ -5,9 +6,51 @@ function funcionCaja() {
     const pokemons = document.getElementById("pokemons");
     const namePokemon = document.getElementById("poke1");
     const value = namePokemon.value;
+
     if (value != "") {
         const li = document.createElement("li");
         li.innerHTML = namePokemon.value;
         namePokemon.value = "";
         pokemons.appendChild(li);
+
+
+        const button = document.createElement("button");
+        button.innerHTML = "Eliminar";
+        button.addEventListener("click", function() {
+            pokemons.removeChild(li)
+        });
+        li.appendChild(button);
+
     }
+}
+
+
+
+
+
+
+
+
+
+/*
+//Boton Delete
+const deleteButton = document.querySelector(".contenedor button");
+deleteButton.addEventListener("click", funcionBorrar);
+
+function funcionDelete(){
+    Reflect.defineProperty(funcionCaja);
+
+}
+
+
+function funcionDelete(){
+    const namePokemon = document.getElementById("poke1");
+    namePokemon.parentNode.removeChild(namePokemon);
+}
+
+*/
+
+
+
+
+
